dad74416d64e95f54994d2e1a360a2d3ecf609ac
nosihle <thulanit@seas.upenn.edu>
2013-12-01 08:58:15 -0500

    Create localization.h

---
+++ localization.h
@@ -0,0 +1,17 @@
+#ifndef localization_h
+#define localization_h
+
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "m_general.h"
+#include "m_usb.h"
+#include "m_bus.h"
+#include "m_wii.h"
+
+// Define functions
+int localize(signed char location[]); // function to find position and orientation
+
+#endif
