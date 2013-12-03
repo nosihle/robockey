82bff34f8c01c38fb3a015e2743e5b66e315aac1
solomong <s.gardner469@gmail.com>
2013-12-02 20:53:12 -0500

    Update motor.c

--- motor.c
+++ motor.c
@@ -4,25 +4,21 @@
 void fwd(void){
 	set(MOTOR,LMOTOR_D);
 	set(MOTOR,RMOTOR_D);
-	set(MOTOR,MOTOR_EN);
 }
 
 void rev(void){
 	clear(MOTOR,LMOTOR_D);
 	clear(MOTOR,RMOTOR_D);
-	set(MOTOR,MOTOR_EN);
 }
 
 void right(void){
 	set(MOTOR,LMOTOR_D);
 	clear(MOTOR,RMOTOR_D);
-	set(MOTOR,MOTOR_EN);
 }
 
 void left(void){
 	clear(MOTOR,LMOTOR_D);
 	set(MOTOR,RMOTOR_D);
-	set(MOTOR,MOTOR_EN);
 }
 
 void stop(void){
